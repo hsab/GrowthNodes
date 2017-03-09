e3f1cb5166e8db0f297018d2e2081d84d79d4f81
mp4 <therealmarshpoulson@gmail.com>
2017-03-08 21:26:57 -0700

    some basic ui elements some unnecessary items

--- __init__.py
+++ __init__.py
@@ -31,20 +31,92 @@ class MyCustomNode(bpy.types.Node):
 
 class HelloWorldPanel(bpy.types.Panel):
 	bl_idname = "panel.panel3"
-	bl_label = "Panel3"
+	bl_label = "UMOG"
 	bl_space_type = "NODE_EDITOR"
-	bl_region_type = "UI"
+	bl_region_type = "TOOLS"
+	bl_category = "UMOG"
 
 	def draw(self, context):
-		self.layout.operator("mesh.add_cube_sample", icon='MESH_CUBE', text="Add Cube 3")
+		self.layout.operator("mesh.add_cube_sample", icon='RENDER_RESULT', text="Bake Mesh(es)")
+
+class UMOGNode(bpy.types.Node):
+		bl_width_min = 10
+		bl_width_max = 5000
+		_IsUMOGNode = True
+		
+		@classmethod
+		def poll(cls, nodeTree):
+			return nodeTree.bl_idname == "umog_UMOGNodeTree"
+		def create(self):
+			pass
+		
+		def init(self, context):
+			self.create()
+
+class addCubeSample(bpy.types.Operator):
+    bl_idname = 'mesh.add_cube_sample'
+    bl_label = 'Add Cube'
+    bl_options = {"REGISTER", "UNDO"}
+ 
+    def execute(self, context):
+        bpy.ops.mesh.primitive_cube_add()
+        return {"FINISHED"}
+			
+class UMOGMeshInputNode(UMOGNode):
+	bl_idname = "umog_MeshInputNode"
+	bl_label = "UMOG Mesh Input Node"
+
+	def create(self):
+		self.newInput("BMesh", "BMesh", "bm")
+
+		self.newOutput("Vector List", "Vertex Locations", "vertexLocations")
+		self.newOutput("Edge Indices List", "Edge Indices", "edgeIndices")
+		self.newOutput("Polygon Indices List", "Polygon Indices", "polygonIndices")
+		self.newOutput("Vertex List", "Vertices", "vertices")
+		self.newOutput("Polygon List", "Polygons", "polygons")
+		
+class UMOGNodeTree(bpy.types.NodeTree):
+    bl_idname = "umog_UMOGNodeTree"
+    bl_label = "UMOG"
+    bl_icon = "SCULPTMODE_HLT"
+
+def drawMenu(self, context):
+	if context.space_data.tree_type != "umog_UMOGNodeTree": return
 	
+	layout = self.layout
+	layout.operator_context = "INVOKE_DEFAULT"
+	layout.menu("umog_mesh_menu", text="Mesh", icon = "MESH_DATA")
+# from animation nodes
+def insertNode(layout, type, text, settings = {}, icon = "NONE"):
+	operator = layout.operator("node.add_node", text = text, icon = icon)
+	operator.type = type
+	operator.use_transform = True
+	for name, value in settings.items():
+		item = operator.settings.add()
+		item.name = name
+		item.value = value
+	return operator
+	
+#todo creat the menu class
+class UMOGMeshMenu(bpy.types.Menu):
+	bl_idname = "umog_mesh_menu"
+	bl_label = "Mesh Menu"
+	
+	def draw(self, context):
+		layout = self.layout
+		insertNode(layout, "umog_MeshInputNode", "Input Mesh")
+
+
 def register():
 	print("begin resitration")
+	bpy.types.NODE_MT_add.append(drawMenu)
 	bpy.utils.register_class(MyCustomNode)
 	bpy.utils.register_class(HelloWorldPanel)
+	bpy.utils.register_class(UMOGNodeTree)
 	bpy.utils.register_module(__name__)
 
 def unregister():
+	bpy.types.NODE_MT_add.remove(drawMenu)
 	bpy.utils.unregister_module(__name__)
 	
 if __name__ == "__main__":
