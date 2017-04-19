883c0aa3d7c9ae7a21c69742c82f115d9e1f154b
mp4 <therealmarshpoulson@gmail.com>
2017-04-19 14:58:50 -0600

    stubbed out sculpt node includes getting the texture handle from the attachable texture node

--- __init__.py
+++ __init__.py
@@ -79,6 +79,7 @@ class UMOGMeshMenu(bpy.types.Menu):
             insertNode(layout, "umog_GetTextureNode", "Texture")
             insertNode(layout, "umog_NoiseGenerationNode", "Noise Generator")
             insertNode(layout, "umog_Mat3Node", "Matrix 3x3")
+            insertNode(layout, "umog_SculptNode", "Sculpt Node")
 
 def register():
     print("begin resitration")
