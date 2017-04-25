f2ed5792e9f56bae97c496e064d11bdf8853b6c5
hsab <hsab@users.noreply.github.com>
2017-04-25 05:31:43 -0600

    major cleanup, bug fix, demo ready

--- sockets.py
+++ sockets.py
@@ -1,50 +1,29 @@
+import bpy
 from bpy.types import NodeSocket
 
 ################################
 # START: Sockets
-class GetTextureSocket(NodeSocket):
+class GetBaseInputSocket(NodeSocket):
     # Description string
     '''Custom Texture socket type'''
     # Optional identifier string. If not explicitly defined, the python class name is used.
-    bl_idname = 'GetTextureSocketType'
+    bl_idname = 'BaseInputSocketType'
     # Label for nice name display
-    bl_label = 'Get Texture Socket'
+    bl_label = 'Get Input Socket'
+    
+    objectName = bpy.props.StringProperty()
     
     def init(self, context):
         pass
         
     # Optional function for drawing the socket input value
     def draw(self, context, layout, node, text):
-#        if self.is_output or self.is_linked:
-#            layout.label(text)
-#        else:
         layout.label(text=text)
+
 
     # Socket color
     def draw_color(self, context, node):
         return (1, 1, 1, 0.5)
-    
-class GetObjectSocket(NodeSocket):
-    # Description string
-    '''Custom node socket type'''
-    # Optional identifier string. If not explicitly defined, the python class name is used.
-    bl_idname = 'GetObjectSocketType'
-    # Label for nice name display
-    bl_label = 'Get Object Socket'
-    
-    def init(self, context):
-        pass
-        
-    # Optional function for drawing the socket input value
-    def draw(self, context, layout, node, text):
-#        if self.is_output or self.is_linked:
-#            layout.label(text)
-#        else:
-        layout.label(text=text)
-
-    # Socket colorblender python Property
-    def draw_color(self, context, node):
-        return (1.0, 0.4, 0.216, 0.5)
     
 class Mat3Socket(NodeSocket):
     # Description string
