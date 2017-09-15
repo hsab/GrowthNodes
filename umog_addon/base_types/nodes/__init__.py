a1c272ba68582e137f23111c1e896d6581c36738
hsab <hsab@users.noreply.github.com>
2017-09-15 05:12:06 -0600

    reorganized node files

---
+++ umog_addon/base_types/nodes/__init__.py
@@ -0,0 +1,2 @@
+from . base_node import UMOGNode
+from . sync_only_node import UMOGOutputNode
---
+++ umog_addon/nodes/algorithm/convolve.py
@@ -0,0 +1,33 @@
+from ... base_types import UMOGNode
+import numpy as np
+import pyximport
+pyximport.install()
+from ...events import events
+
+class ConvolveNode(UMOGNode):
+    bl_idname = "umog_ConvolveNode"
+    bl_label = "Convolve Node"
+
+    def init(self, context):
+        self.outputs.new("TextureSocketType", "out")
+        self.inputs.new("TextureSocketType", "in")
+        super().init(context)
+
+    def draw_buttons(self, context, layout):
+        pass
+
+    def update(self):
+        pass
+
+    def execute(self, refholder):
+        # compute A'
+        print("convolve node")
+        mask = np.array([[0.05, 0.2, 0.05], [0.2, -1, 0.2], [0.05, 0.2, 0.05]])
+        A = refholder.np2dtextures[self.inputs[0].links[0].from_socket.texture_index]
+        Ap = refholder.np2dtextures[self.outputs[0].texture_index]
+        events.convolve2d(A, mask, Ap)
+
+        print(" convolve min, max " + str(np.amin(Ap)) + "," + str(np.amax(Ap)))
+
+    def preExecute(self, refholder):
+        self.outputs[0].texture_index = refholder.createRefForTexture2d()
