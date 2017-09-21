b1e50b2793e50beec47f9a1f93b2f4a4792ee112
hsab <hsab@users.noreply.github.com>
2017-09-21 14:30:06 -0600

    refactor execute(); add write_keyframe()

--- umog_addon/operators/bake_meshes.py
+++ umog_addon/operators/bake_meshes.py
@@ -9,61 +9,12 @@ class bakeMeshes(bpy.types.Operator):
     bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context):
-        start_nodes = []
-        # initialize NodePriority to -1 for non output and 0 for output nodes
-        nn2p = {}
-        # dictionary of enabled
-        nn2e = {}
-        for node in bpy.context.space_data.edit_tree.nodes:
-            try:
-                if node._OutputNode:
-                    nn2p[node.name] = 0
-                    start_nodes.append(node)
-            except:
-                nn2p[node.name] = -1
-            nn2e[node.name] = True
-
-        # now using the start nodes
-        while len(start_nodes) != 0:
-            next_nodes = []
-            for node in start_nodes:
-                for ln in node.inputs:
-                    try:
-                        ln = ln.links[0].from_node
-                        if nn2p[ln.name] != 0:
-                            nn2p[ln.name] = nn2p[node.name] + 1
-                            next_nodes.append(ln)
-                    except:
-                        pass
-            start_nodes = next_nodes
-        # sort the nodes by NodePriority
-        sorted_nodes = sorted(bpy.context.space_data.edit_tree.nodes, key=lambda node: nn2p[node.name])
-        # highest numbered nodes should be first
-        sorted_nodes.reverse()
-
-        for node in sorted_nodes:
-            if nn2p[node.name] == -1:
-                nn2e[node.name] = False
-
-        refholder = UMOGReferenceHolder()
+        node_tree = context.space_data.edit_tree
         
         start_time = time.time()
-        
-        for node in sorted_nodes:
-            if nn2e[node.name]:
-                node.preExecute(refholder)
-        for frames in range(bpy.context.scene.StartFrame, bpy.context.scene.EndFrame):
-            for subframes in range(0, bpy.context.scene.SubFrames):
-                for node in sorted_nodes:
-                    if nn2e[node.name]:
-                        node.execute(refholder)
-            for node in sorted_nodes:
-                if nn2e[node.name]:
-                    node.postFrame(refholder)
-                    # consider at what point to do the end of frame calls
-        for node in sorted_nodes:
-            if nn2e[node.name]:
-                node.postBake(refholder)
+
+        refholder = UMOGReferenceHolder()
+        node_tree.execute(refholder, context.scene.StartFrame, context.scene.EndFrame, context.scene.SubFrames)
                 
         diff_time = time.time() - start_time
         print("the bake took " + str(diff_time))
