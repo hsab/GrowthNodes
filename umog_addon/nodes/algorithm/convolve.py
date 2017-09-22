05211a8007d1dd7a090897f0ee2b841a69d95fe7
hsab <hsab@users.noreply.github.com>
2017-09-21 23:37:14 -0600

    Added some basic matrix nodes. I/O working between matrices. Changed convulation to take in matrix input instead of default.

--- umog_addon/nodes/algorithm/convolve.py
+++ umog_addon/nodes/algorithm/convolve.py
@@ -10,7 +10,8 @@ class ConvolveNode(UMOGNode):
 
     def init(self, context):
         self.outputs.new("TextureSocketType", "out")
-        self.inputs.new("TextureSocketType", "in")
+        self.inputs.new("TextureSocketType", "Texture Input")
+        self.inputs.new("Mat3SocketType", "Matrix Input")
         super().init(context)
 
     def draw_buttons(self, context, layout):
@@ -22,7 +23,7 @@ class ConvolveNode(UMOGNode):
     def execute(self, refholder):
         # compute A'
         print("convolve node")
-        mask = np.array([[0.05, 0.2, 0.05], [0.2, -1, 0.2], [0.05, 0.2, 0.05]])
+        mask = refholder.matrices[self.inputs[1].links[0].from_socket.matrix_ref]
         A = refholder.np2dtextures[self.inputs[0].links[0].from_socket.texture_index]
         Ap = refholder.np2dtextures[self.outputs[0].texture_index]
         events.convolve2d(A, mask, Ap)
