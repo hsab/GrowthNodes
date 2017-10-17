d468eab3f427c5a1660277be732701ba95ede688
Micah Johnston <micah@glowcoil.com>
2017-10-17 00:08:41 -0600

    UI changes to move towards compilation

--- umog_addon/panel.py
+++ umog_addon/panel.py
@@ -8,11 +8,4 @@ class UMOGNodeEditorPanel(bpy.types.Panel):
     bl_category = "UMOG"
 
     def draw(self, context):
-        self.layout.operator("umog.bake_meshes", icon='RENDER_RESULT', text="Bake Mesh(es)")
-        self.layout.operator("umog.render_animation", icon='RENDER_ANIMATION', text="Render Animation")
-        self.layout.operator("umog.run_node_tree", icon='RENDER_ANIMATION', text="Run")
-        self.layout.prop(bpy.context.scene, 'StartFrame')
-        self.layout.prop(bpy.context.scene, 'EndFrame')
-        self.layout.prop(bpy.context.scene, 'SubFrames')
-        self.layout.prop(bpy.context.scene, 'TextureResolution')
-
+        self.layout.operator("umog.run_node_tree", icon='RENDER_RESULT', text="Run")
