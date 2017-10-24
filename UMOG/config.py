8c56c837319b82d7745d352fea491e737cbec22a
hsab <hsab@users.noreply.github.com>
2017-10-24 15:08:23 -0600

    dumb (removed copy of build fold)

--- UMOG/config.py
+++
@@ -1,3 +0,0 @@
-addonsDirectory = "C:\\Users\\Hirad\\AppData\\Roaming\\Blender Foundation\\Blender\\2.79\\scripts\\addons\\UMOG\\test"
-skipFiles = ['config.py', 'config.default.py', 'compilation_info.json', 'setup.py', 'umog.zip']
-skipFolders = {".git", ".idea", "__pycache__", "build", "playground"}
