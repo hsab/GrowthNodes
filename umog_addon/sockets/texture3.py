afc570487b8cd5293c6f58a25b1236652877567c
mp4 <therealmarshpoulson@gmail.com>
2017-11-01 20:24:56 +0000

    added texture3 socket type

--- umog_addon/sockets/texture3.py
+++ umog_addon/sockets/texture3.py
@@ -8,22 +8,22 @@ from ..base_types import UMOGSocket
 from ..utils.events import propUpdate
 
 
-class UMOGTextureData(dict):
-    bl_idname = "umog_TextureData"
+class UMOGTexture3Data(dict):
+    bl_idname = "umog_Texture3Data"
 
     def __init__(self):
         pass
 
 
-class Texture2Socket(bpy.types.NodeSocket, UMOGSocket):
+class Texture3Socket(bpy.types.NodeSocket, UMOGSocket):
     # Description string
     '''Custom Float socket type'''
     # Optional identifier string. If not explicitly defined, the python class name is used.
-    bl_idname = 'Texture2SocketType'
+    bl_idname = 'Texture3SocketType'
     # Label for nice name display
-    bl_label = 'Texture2 Socket'
-    dataType = "Texture2"
-    allowedInputTypes = ["Texture2"]
+    bl_label = 'Texture3 Socket'
+    dataType = "Texture3"
+    allowedInputTypes = ["Texture3"]
 
     useIsUsedProperty = False
     defaultDrawType = "PREFER_PROPERTY"
@@ -32,85 +32,17 @@ class Texture2Socket(bpy.types.NodeSocket, UMOGSocket):
 
     value = StringProperty(update = propUpdate)
 
-    data = UMOGTextureData()
+    data = UMOGTexture3Data()
 
     def drawProperty(self, context, layout, layoutParent, text, node):
-        layout.prop_search(self, "value", bpy.data, "textures", icon = "TEXTURE_DATA",
-                           text = "")
-        if self.value is not "":
-            pass
+        pass
 
     def pack(self):
         resolution = self.nodeTree.properties.TextureResolution
         if self.isOutput:
-            self.initOutputData(resolution)
+            pass
         else:
-            self.packInputData(resolution)
-
-    def initOutputData(self, resolution):
-        D = bpy.data
-        textureName = self.nodeTree.name + " - " + self.node.name + " - " + str(
-            self.index)
-        if textureName not in D.textures:
-            texture = D.textures.new(textureName, "IMAGE")
-        else:
-            texture = D.textures[textureName]
-
-        if textureName in D.images:
-            D.images.remove(D.images[textureName])
-
-        image = D.images.new(textureName, resolution, resolution, alpha = False,
-                             float_buffer = True)
-
-        texture.image = image
-        self.value = texture.name
-
-    def packInputData(self, resolution):
-        rows = resolution
-        columns = resolution
-        # TODO: Fix harcoded channel attr
-        pixels = np.empty((rows, columns, 4), dtype = "float")
-        fromTexture = self.getTexture()
-
-        if fromTexture.type != "IMAGE":
-            self.proceduralToNumpy(fromTexture, pixels, rows, columns)
-        else:
-            self.imageToNumpy(fromTexture, pixels, rows, columns)
-
-        self.data[self.identifier] = pixels
-
-    def proceduralToNumpy(self, fromTexture, pixels, rows, columns):
-        for i in range(rows):
-            for j in range(columns):
-                # Mapping from -1 to 1 for evaluate
-                x = (i / rows) * 2 - 1
-                y = (j / columns) * 2 - 1
-                w = fromTexture.evaluate((y, x, 0)).w
-                pixels[i, j][0] = w
-                pixels[i, j][1] = w
-                pixels[i, j][2] = w
-                pixels[i, j][3] = 1.0
-
-        return pixels
-
-    def imageToNumpy(self, fromTexture, pixels, rows, columns):
-        fromImage = fromTexture.image
-        fromRows = fromImage.size[1]
-        fromColumns = fromImage.size[0]
-        fromPixels = np.asarray(fromImage.pixels, dtype = "float")
-        fromPixels = fromPixels.reshape(fromRows, fromColumns, fromImage.channels)
-
-        for i in range(rows):
-            for j in range(columns):
-                # Scale image to resolution*resolution
-                x = int(i * (fromRows / rows))
-                y = int(j * (fromColumns / columns))
-                pixels[i, j][0] = fromPixels[x, y][0]
-                pixels[i, j][1] = fromPixels[x, y][1]
-                pixels[i, j][2] = fromPixels[x, y][2]
-                pixels[i, j][3] = fromPixels[x, y][3]
-
-        return pixels
+            self.data[self.identifier] = np.zeros((resolution, resolution, resolution), dtype = "float")
 
     def setPackedImageFromPixels(self, newPixels, flatten=True):
         if self.isOutput and self.isPacked:
@@ -148,20 +80,8 @@ class Texture2Socket(bpy.types.NodeSocket, UMOGSocket):
     def getPixels(self):
         return self.data[self.identifier]
 
-    def getValue(self):
-        return self.value
-
-    def setProperty(self, data):
-        self.value = data
-
-    def getProperty(self):
-        return self.value
-
     def refresh(self):
-        self.name = self.value
-
-    def getTexture(self):
-        return bpy.data.textures[self.value]
+        self.name = self.identifier
 
     def destroy(self):
         if self.isPacked:
