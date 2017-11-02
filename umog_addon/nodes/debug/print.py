1615811986ca49be244d76075bea86c72a6b9c57
hsab <hsab@users.noreply.github.com>
2017-11-02 13:12:41 -0600

    add print node for debugging

---
+++ umog_addon/nodes/debug/print.py
@@ -0,0 +1,31 @@
+from ..umog_node import *
+from ...engine import types, engine, mesh
+import bpy
+
+class PrintNode(UMOGOutputNode):
+    bl_idname = "umog_PrintNode"
+    bl_label = "Print Node"
+
+    def init(self, context):
+        self.inputs.new("FloatSocketType", "value")
+        super().init(context)
+
+    def draw_buttons(self, context, layout):
+        pass
+
+    def get_operation(self):
+        return engine.Operation(
+            engine.OUT,
+            [types.Scalar()],
+            [],
+            [],
+            [])
+
+    def get_buffer_values(self):
+        return []
+
+    def output_value(self, value):
+        print(value)
+
+    def update(self):
+        pass
