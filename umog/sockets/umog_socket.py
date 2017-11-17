c2d78bac51a4b1d60c215082c25aeac87e6774db
hsab <hsab@users.noreply.github.com>
2017-11-16 18:54:27 -0700

    remove unused code

--- umog/sockets/umog_socket.py
+++ umog/sockets/umog_socket.py
@@ -1,7 +1,6 @@
 import bpy
 from bpy.props import *
 from collections import defaultdict
-from .. utils.recursion import noRecursion
 from .. operators.callbacks import newSocketCallback
 from .. utils.names import getRandomString, toVariableName
 from .. operators.dynamic_operators import getInvokeFunctionOperator
@@ -125,26 +124,6 @@ class UMOGSocket(bpy.types.NodeSocket):
 
     def getProperty(self):
         return
-
-    @classmethod
-    def getDefaultValue(cls):
-        raise NotImplementedError(
-            "All sockets have to define a getDefaultValue method")
-
-    @classmethod
-    def correctValue(cls, value):
-        '''
-        Return Types:
-          If the value has the correct type: (value, 0)
-          If the value has a correctable type: (corrected_value, 1)
-          if the value has a uncorrectable type: (default_value, 2)
-        '''
-        raise NotImplementedError(
-            "All sockets have to define a correctValue method")
-
-    @classmethod
-    def getConversionCode(cls, dataType):
-        return None
 
     # Drawing
     ##########################################################
@@ -476,11 +455,8 @@ class UMOGSocket(bpy.types.NodeSocket):
     def getCopyExpression(cls):
         return "value[:]"
 
-
-@noRecursion
 def updateText(socket):
     correctText(socket)
-
 
 def correctText(socket):
     if socket.textProps.variable:
