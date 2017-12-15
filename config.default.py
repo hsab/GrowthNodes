2c88afcb0dfc580e8e006960c86c8d4c12576272
hsab <hsab@users.noreply.github.com>
2017-12-15 15:17:51 -0700

    final cleanup

--- config.default.py
+++ config.default.py
@@ -1,3 +1,3 @@
-addonsDirectory = "C:\\Users\\Hirad\\AppData\\Roaming\\Blender Foundation\\Blender\\2.79\\scripts\\addons" # your Blender addons directory here
+addonsDirectory = "" # your Blender addons directory here
 skipFiles = ['config.py', 'config.default.py', 'compilation_info.json', 'setup.py', 'umog.zip']
 skipFolders = {".git", ".idea", "__pycache__", "build", "playground"}
